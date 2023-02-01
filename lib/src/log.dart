fd918cadf3036ef1f004dc3c3cc0fcdf048e112d
Andrii Syrokomskyi <signmotion@gmail.com>
2023-02-01 18:00:00 +0100

    Added a truncate log output for long string.

--- lib/src/log.dart
+++ lib/src/log.dart
@@ -1,6 +1,14 @@
 typedef Log = void Function(String);
 
 /// U+1F343
-void li(String s) => print('🍃 $s');
+void li(String s) => print('🍃 ${s.truncated}');
 
 void liSilent(String s) {}
+
+const _truncatedLength = 300;
+
+extension _StringExtension on String {
+  String get truncated => _truncatedLength >= length
+      ? this
+      : replaceRange(_truncatedLength, length, '…');
+}
